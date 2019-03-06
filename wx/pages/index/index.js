222c99484c65d6678a2b6ba3c66e0ad55ed64819
JacksonWang <JacksonWang@JacksonWang-PC>
2019-03-06 22:45:30 +0800

    wangjisen

--- wx/pages/index/index.js
+++ wx/pages/index/index.js
@@ -1,12 +1,94 @@
 //index.js
 //获取应用实例
 const app = getApp()
+//获取工具包
+var util = require("../../utils/util.js");
 
 Page({
   data: {
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    classArray: ["一年级一班","一年级二班","二年级一班","二年级二班"],
+    objectClassArray: [
+      {
+        id: 0,
+        name: '一年级一班'
+      },
+      {
+        id: 1,
+        name: '一年级二班'
+      },
+      {
+        id: 2,
+        name: '二年级一班'
+      },
+      {
+        id: 3,
+        name: '二年级二班'
+      }
+    ],
+    classIndex: 0,
+    timeArray: [['周一', '周二', '周三', '周四', '周五', '周六', '周日'], ['第一二节课', '第三四节课', '第五六节课', '第七八节课', '第九十节课']],
+    objectTimeArray: [
+      [
+        {
+          id: 0,
+          name: '周一'
+        },
+        {
+          id: 1,
+          name: '周二'
+        },
+        {
+          id: 2,
+          name: '周三'
+        },
+        {
+          id:3 ,
+          name: '周四'
+        },
+        {
+          id:4 ,
+          name: '周五'
+        },
+        {
+          id: 5,
+          name: '周六'
+        },
+        {
+          id: 6,
+          name: '周日'
+        }
+      ], [
+        {
+          id: 0,
+          name: '第一二节课'
+        },
+        {
+          id: 1,
+          name: '第三四节课'
+        },
+        {
+          id: 2,
+          name: '第五六节课'
+        },
+        {
+          id: 3,
+          name: '第七八节课'
+        },
+        {
+          id: 4,
+          name: '第九十节课'
+        }
+      ]
+    ],
+    timeIndex: [0, 0],
+    equipmentItems: [
+      { name: '0', value: '单片机' },
+      { name: '1', value: '电路板' },
+      { name: '2', value: '电烙铁' }
+    ],
     access_token: ''
   },
 
@@ -55,6 +137,7 @@ Page({
     });
   },
 
+  //获取当前用户信息
   getUserInfo: function (e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
@@ -64,18 +147,63 @@ Page({
     })
   },
 
+  //班级选择器
+  bindClassChange(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      classIndex: e.detail.value
+    })
+  },
+
+  //时间选择器
+  bindTimeChange(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      timeIndex: e.detail.value
+    })
+  },
+
+  //器材选择器
+  bindEquipmentChange(e) {
+    console.log('checkbox发生change事件，携带value值为：', e.detail.value)
+  },
+
   //提交表单
-  formSubmit(e) {
+  formSubmit: function(e) {
+    //获取提交表单的当前时间
+    var datetime = util.formatTime(new Date());
+    console.log("Current datetime = "+datetime);
+
     //获取input文本框的值
-    var txt = e.detail.value.text;
-    console.log('form发生了submit事件，携带数据为：', txt);
+    var formData = e.detail.value;
+    console.log('form发生了submit事件，携带数据为：', formData);
+
+    //提交表单
+    wx.request({
+      url: 'http://localhost:8080/laboratory/laboratory/saveMonad',
+      data: {
+        "cls": formData.cls,
+        "stuName": formData.name,
+        "time": formData.time,
+        "date": util.formatTime,
+        "equipment": formData.equipment,
+        "note": formData.note
+      },
+      method: 'post',
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success: function (res) {
+        console.log("Save monad successfully. res = "+res.data);
+      }
+    });
 
     var _this = this;
     console.log(_this.data.access_token, 'access_token');
     console.log(app.globalData.openid, 'openid');
     //formid是设置了form的属性report-submit为true时，通过e.detail.formId获取
     console.log(e.detail.formId, 'formid');
-    //发起请求
+    //发起请求，发送模板信息
     wx.request({
       url: 'https://api.weixin.qq.com/cgi-bin/message/wxopen/template/send?access_token=' + _this.data.access_token,
       data: {
@@ -84,7 +212,7 @@ Page({
         "form_id": e.detail.formId,
         "data": {
           "k eyword1": {
-            "value": "2019-2-21",
+            "value": datetime,
             "color": "#173177"
           },
           "keyword2": {
@@ -92,11 +220,11 @@ Page({
             "color": "#173177"
           },
           "keyword3": {
-            "value": txt,
+            "value": formData.equipment,
             "color": "#173177"
           },
           "keyword4": {
-            "value": "楷兵",
+            "value": userInfo.nickName,
             "color": "#173177"
           }
         },
@@ -109,6 +237,6 @@ Page({
       success: function (res) {
         console.log(res.data)
       }
-    })
+    });
   }
 })
--- wx/utils/util.js
+++ wx/utils/util.js
@@ -1,12 +1,30 @@
 const formatTime = date => {
   const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
+  const month = formatNum(date.getMonth() + 1);
+  const day = formatNum(date.getDate());
+  const hour = formatNum(date.getHours());
+  const minute = formatNum(date.getMinutes());
+  const second = formatNum(date.getSeconds());
 
-  return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+  return [year, month, day].map(formatNumber).join('-') + ' ' + [hour, minute, second].map(formatNumber).join(':');
+}
+
+//自定义格式化日期
+const formatDate = date => {
+  const year = date.getFullYear()
+  const month = formatNum(date.getMonth() + 1);
+  const day = formatNum(date.getDate());
+
+  return [year, month, day].map(formatNumber).join('-')
+}
+
+//自定义格式化两位数
+const formatNum = num => {
+  //若传入的数字在0~9的范围内，则拼凑成两位数
+  if(num>=0 && num<=9) {
+    return "0"+num;
+  }
+  return num;
 }
 
 const formatNumber = n => {
